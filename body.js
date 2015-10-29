fe68c91ac7b7d619bb2d3434a8972d59c2427697
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2015-10-29 16:33:58 -0400

    Initial commit

---
+++ body.js
@@ -0,0 +1,6 @@
+(function(){
+  ((window.gitter = {}).chat = {}).options = INSTALL_OPTIONS;
+  var script = document.createElement('script')
+  script.src = 'https://sidecar.gitter.im/dist/sidecar.v1.js'
+  document.body.appendChild(script)
+})();
